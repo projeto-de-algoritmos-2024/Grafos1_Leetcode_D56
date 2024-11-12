0e67aac4e71273ea40f87e6026d07303604b9989
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2024-11-11 22:04:16 -0300

    Primeira tentativa - 207
    Tentativa implementando DFS, percorrendo o grafo de pre requisitos, iniciando os visitados vazios

--- 207_Course Schedule/207.py
+++ 207_Course Schedule/207.py
@@ -1,8 +1,23 @@
 class Solution(object):
+    def dfs(self, cursos, visitado, no):
+            visitado[no] = False
+            for vizinho in cursos[no]:
+                if self.dfs(cursos, visitado, vizinho) == False:
+                    return False
+            visitado[no] = True
+            return True
+
     def canFinish(self, numCourses, prerequisites):
         """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
+        :type numCursos: int
+        :type preRequisitos: List[List[int]]
         :rtype: bool
         """
-        
+        cursos = {i: [] for i in range(numCourses)}
+        for x, y in prerequisites:
+            cursos[x].append(y)
+        visitado = [None] * numCourses
+        for i in range(numCourses):
+            if dfs(cursos, visitado, i):
+                return False
+        return True
