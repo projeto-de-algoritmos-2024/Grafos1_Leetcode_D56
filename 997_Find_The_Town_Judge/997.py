30ad4202bfb09eede5c2f1874437b6b0197be589
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2024-11-11 23:26:52 -0300

    Tentativa 1

--- 997_Find_The_Town_Judge/997.py
+++ 997_Find_The_Town_Judge/997.py
@@ -8,3 +8,12 @@ class Solution(object):
         from collections import defaultdict
         confianca = defaultdict(int)
         confiados = defaultdict(int)
+
+        for x, y in trust:
+            confiados[x] += 1
+            confianca[y] =- 1
+
+        for i in range(1, n + 1):
+            if confianca.get(i, 0) == 0 and confiados.get(i, 0) == n - 1:
+                return i 
+        return -1
